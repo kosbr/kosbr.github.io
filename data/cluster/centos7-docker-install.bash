6ca3a9cd09a084ca139f47d12c66d110915fe4f2
kosbr <kosbr.dev@yandex.com>
2017-09-08 17:46:27 +0700

    comment firewalld commands

--- data/cluster/centos7-docker-install.bash
+++ data/cluster/centos7-docker-install.bash
@@ -14,12 +14,12 @@ yum makecache fast
 yum install -y docker-ce
 
 # Open ports
-firewall-cmd --add-port=2376/tcp --permanent
-firewall-cmd --add-port=2377/tcp --permanent
-firewall-cmd --add-port=7946/tcp --permanent
-firewall-cmd --add-port=7946/udp --permanent
-firewall-cmd --add-port=4789/udp --permanent
+#firewall-cmd --add-port=2376/tcp --permanent
+#firewall-cmd --add-port=2377/tcp --permanent
+#firewall-cmd --add-port=7946/tcp --permanent
+#firewall-cmd --add-port=7946/udp --permanent
+#firewall-cmd --add-port=4789/udp --permanent
 
-firewall-cmd --reload
+#firewall-cmd --reload
 
 systemctl start docker
